a9d37b288bd4d64bdf68954f7e0e2f6f30aff78e
Dr. Thomas Jacobi <62063372+Timsbim@users.noreply.github.com>
2024-01-16 17:00:50 +0100

    Update __init__.py

--- util/__init__.py
+++ util/__init__.py
@@ -1,3 +1,3 @@
-from util.basics import set_date
-from util.prepping import download_data, prepare_data
-from util.showing import show_countries, show_groups
+from utils.basics import set_date
+from utils.prepping import download_data, prepare_data
+from utils.showing import show_countries, show_groups
